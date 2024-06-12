16ef19c848f72644898bd19d4725bad2aca0ba82
DESKTOP-CCDS2EU\natas <natasha_sheko@yahoo.com>
2024-06-12 22:44:59 +0200

    design changes only

--- src/pages/ProjectTable.tsx
+++ src/pages/ProjectTable.tsx
@@ -25,38 +25,39 @@ type PropsType = {
 export default function ProjectTable({id, time, businessDesc, stack, place}: PropsType) {
     const [open, setOpen] = useState(false);
 
-    return <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
+    return <TableContainer component={Paper} className='table-header'>
+        <Table sx={{ minWidth: 650 }}  className='table-header'>
+
                 <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell>Zeit</TableCell>
-                    <TableCell>Ort</TableCell>
-                    <TableCell>fachliche Beschreibung</TableCell>
+                    <TableCell className='table-header'></TableCell>
+                    <TableCell className='table-header'>Zeit</TableCell>
+                    <TableCell className='table-header'>Ort</TableCell>
+                    <TableCell className='table-header'>fachliche Beschreibung</TableCell>
                 </TableRow>
-            </TableHead>
-            <TableBody>
+
+            <TableBody className='table-body'>
                 <TableRow
                     key={id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                    <TableCell>
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }} className='table-body'>
+
+                    <TableCell className='table-body'>
                         <IconButton
-                            aria-label="expand row"
-                            size="small"
-                            onClick={() => setOpen(!open)}
-                        >
+                            aria-label='expand row'
+                            size='small'
+                            sx={{backgroundColor:'#6d9a8b'}}
+                            onClick={() => setOpen(!open)}>
                             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                         </IconButton>
                     </TableCell>
-                    <TableCell component="th" scope="row">{time}</TableCell>
-                    <TableCell align="left">{place}</TableCell>
-                    <TableCell align="left">{businessDesc}</TableCell>
+                    <TableCell component="th" scope="row" className='table-body'>{time}</TableCell>
+                    <TableCell align="left" className='table-body'>{place}</TableCell>
+                    <TableCell align="left" className='table-body'>{businessDesc}</TableCell>
                 </TableRow>
                 <TableRow>
-                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={4}>
+                    <TableCell className='table-collapsible' colSpan={4}>
                         <Collapse in={open} timeout="auto" unmountOnExit>
-                            <Box sx={{ margin: 1 }} >
-                                <Typography  gutterBottom component="div">
+                            <Box sx={{ margin: 1 }}>
+                                <Typography  gutterBottom component="div" >
                                     {stack.split("&").map((curs, idx) => (
                                         <Typography style={{fontSize: '0.875rem'}} key={idx}>{curs}</Typography>
                                     ))}
