498f8b6ec732c8fc3656952a435e2ea8205399da
DESKTOP-CCDS2EU\natas <natasha_sheko@yahoo.com>
2024-06-13 21:35:05 +0200

    header with menu is ready

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState, MouseEvent} from "react";
 import {useNavigate} from "react-router-dom";
-import {AppBar, Box, Button, IconButton, Link, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Button, IconButton, Menu, MenuItem, MenuList, Toolbar} from "@mui/material";
 import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
 
     const navigate = useNavigate();
+    const [anchorNav, setAnchorNav] = useState<null | HTMLElement>(null);
+
+    const openMenu = (event: MouseEvent<HTMLElement>) =>{
+        setAnchorNav(event.currentTarget);
+    }
+    const closeMenu = (event: MouseEvent<HTMLElement>) =>{
+        setAnchorNav(null);
+    }
     const navigateToProjekte = () => {
         navigate('/projekte');
     }
@@ -19,17 +28,45 @@ export default function Header() {
         navigate('/kontakt');
     }
 
+    const links = [
+        {buttonName : "Home", buttonLink: navigateToDashboard},
+        {buttonName : "Projekte", buttonLink: navigateToProjekte},
+        {buttonName : "Ausbildung", buttonLink: navigateToAusbildung},
+        {buttonName : "Kontakt", buttonLink: navigateToContact},
+    ];
+
     return (
         <AppBar style={{backgroundColor:'var(--gray)'}}>
             <Toolbar className={`navbar-wrap`}>
-                <IconButton className={`brand w-nav-brand`} size='large' area-label='logo' edge='start'>
+                <IconButton className={`brand w-nav-brand`} size='large' area-label='logo' edge='start'
+                sx={{display:{xs:'none', md:'flex'}}}>
                     <IntegrationInstructionsIcon/>
                 </IconButton>
-                <Box>
-                    <Button className={`nav-link w-nav-link`} onClick={navigateToDashboard}>Home</Button>
-                    <Button className={`nav-link w-nav-link`} onClick={navigateToProjekte}>Projekte</Button>
-                    <Button className={`nav-link w-nav-link`} onClick={navigateToAusbildung}>Ausbildung</Button>
-                    <Button className={`nav-link w-nav-link`} onClick={navigateToContact}>Kontakt</Button>
+                <Box sx={{display:{xs:'none', md:'flex'}}}>
+                    {links.map(({buttonName, buttonLink}: any) =>
+                        <Button className={`nav-link w-nav-link`} onClick={buttonLink}>{buttonName}</Button>
+                    )}
+                </Box>
+                <Box sx={{display:{xs:'flex', md:'none'}}}>
+                    <IconButton size='large' edge='start' onClick={openMenu}>
+                        <MenuIcon/>
+                    </IconButton>
+                    <Menu open={Boolean(anchorNav)}
+                          sx={{ display:{xs:'flex', md:'none'}, padding:0,  transform: {xs:'translateY(-61%)', sm:'translateY(-67%)'}}}
+                          onClose={closeMenu}
+                          disableAutoFocusItem
+                          >
+                        <MenuList>
+                            {links.map(({buttonName, buttonLink}: any) =>
+                                <MenuItem onClick={buttonLink}>{buttonName}</MenuItem>
+                            )}
+                        </MenuList>
+                    </Menu>
+                    <IconButton className={`brand w-nav-brand`} size='large' area-label='logo' edge='start'
+                                sx={{display:{xs:'flex', md:'none'}}}>
+                        <IntegrationInstructionsIcon/>
+                    </IconButton>
+
                 </Box>
             </Toolbar>
         </AppBar>
